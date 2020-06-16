864a7e9744fb37c4b415bb388d6c646eeca2ee1b
Marcos Nakagawa <nakagawa@gmail.com>
2020-06-15 23:19:11 -0300

    template

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const list = [
+let list = [
   {
   "ID": 1,
     "description": "Importação de arquivos de fundos",
@@ -17,34 +17,83 @@ const list = [
   },
 ]
 
+const labels = {
+  'ID': 'ID',
+  'description': "Descrição",
+  'date_end': "Data máxima de conclusão",
+  'estimate': "Tempo estimado"
+}
+
+const render = () => {
+  const updateValue = (evt) => {
+    const [job, id, key] = evt.target.name.split("-");
+    list[id][key] = evt.target.value;
+  }
+
+  const generateElement = (job, labelText, key, index) => {
+    const field = document.createElement("input");
+    field.value = job[key];
+    field.name = `job-${index}-${key}`;
+    field.id = `job-${index}-${key}`;
+    field.addEventListener('change', updateValue);
+    const label = document.createElement("label");
+    label.innerText = labelText;
+
+    return {field, label}
+  }
+  document.querySelector("#fields").innerHTML = "";
+  list.map((job, index) => {
+    Object.keys(labels).map((key) => {
+      const { field, label } = generateElement(job, labels[key], key, index);
+      document.querySelector("#fields").appendChild(label)
+      document.querySelector("#fields").appendChild(field)
+    })
+    if (index < list.length-1) document.querySelector("#fields").appendChild(document.createElement("hr"))
+  })
+}
+
 const from = new Date("2019-11-10 09:00:00");
 const to = new Date("2019-11-11 12:00:00");
 
-const filter = (item) => {
-  // filters date_end inbetween two dates
-  const date_end = new Date(item.date_end);
-  return (date_end > from && date_end <= to) ? true : false;
+const filterList = (list) => {
+  let count = 0;
+  let index = 0;
+  
+  const queues = [];
+
+  const sumEstimates = (item) => {
+    // sum estimates
+    count += Number(item.estimate);
+    if (queues[index] == undefined) queues[index] = [];
+    queues[index].push(item);
+    if (count >= 8) index++;
+  }
+
+  const filter = (item) => {
+    // filters date_end inbetween two dates
+    const date_end = new Date(item.date_end);
+    return (date_end > from && date_end <= to) ? true : false;
+  }
+  
+  const sort = (a, b) => new Date(a.date_end) > new Date(b.date_end);
+  
+  list.filter(filter)
+    .sort(sort)
+    .map(sumEstimates);
+  
+  return queues;
 }
 
-const sort = (a, b) => new Date(a.date_end) > new Date(b.date_end);
-const sumEstimates = (item) => {
-  // sum estimates
-  count += item.estimate;
-  if (queues[index] == undefined) queues[index] = [];
-  queues[index].push(item.ID);
-  if (count >= 8) index++;
-}
+document.querySelector("#button").addEventListener("click", (evt) => {
+  evt.preventDefault();
+  const result = filterList(list);
+  console.log(result);
+  list = [];
+  render();
+})
 
-let count = 0;
-let index = 0;
+render();
+// console.log(filterList(list));
 
-const queues = [];
 
-list.filter(filter)
-  .sort(sort)
-  .map(sumEstimates);
-
-console.log("result");
-console.log(queues);
-
-document.querySelector("#debug").innerHTML = "output";
+// document.querySelector("#debug").innerHTML = "output";
